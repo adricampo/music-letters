ac0b2d809dea5d6fb3644418dfbc15e0a70dcdfa
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2020-05-13 18:45:06 +0200

    first version of the application done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './components/Formulario'
+import Cancion from './components/Cancion'
+import Artista from './components/Artista'
 
 function App() {
+
+  // definir state
+  const [ busquedaletra, guardarBusquedaLetra ] = useState({})
+  const [ letra, guardarLetra ] = useState("")
+  const [ info, guardarInfo ] = useState({})
+
+  useEffect(() => {
+    // si el objecto resultante de la busqueda viene vacio 
+    if(Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+
+      const { artista, cancion } = busquedaletra
+      const urlCancion = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+      const urlArtista = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`
+      
+      const [ respuestaCancion, respuestaArtista ] = await Promise.all([
+        fetch(urlCancion),
+        fetch(urlArtista)
+      ])
+    
+      const letraCancion = await respuestaCancion.json()
+      const infoArtista = await respuestaArtista.json()
+      
+      // modificamos state con los datos de la letra y el artista (comprobamos que el artista no venga vacio) 
+      guardarLetra(letraCancion.lyrics)
+
+      if(infoArtista.artists === null){
+        return 
+      } else {
+        guardarInfo(infoArtista.artists[0])
+      }
+
+      
+     
+
+      // guardarLetra(resultado.lyrics)
+          
+    }
+    consultarApiLetra()
+
+  }, [busquedaletra, info])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario 
+        guardarBusquedaLetra={guardarBusquedaLetra}
+      />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Artista
+              info={info}
+            />
+          </div>
+          <div className="col-md-6">
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
